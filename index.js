bf48130881c4287372163bc995ef8164f6609f8a
Vanshika Dhingra <vanshika.dhingra@students.iiit.ac.in>
2023-03-08 14:24:42 +0530

    v1

---
+++ index.js
@@ -0,0 +1,26 @@
+// // When using the Tauri global script (if not using the npm package)
+// // Be sure to set `build.withGlobalTauri` in `tauri.conf.json` to true
+const invoke = window.__TAURI__.invoke
+const path = window.__TAURI__.path
+
+// // Invoke the command
+// invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/dass/src-tauri/images' })
+
+// const finalfolderpath='';
+// const folderInput = document.getElementById("folder");
+// folderInput.addEventListener("change", (event) => {
+//   const folderPath = event.target.value;
+//   const folderrr=folderPath;
+//   finalfolderpath = path.dirname(folderrr);
+//   console.log("Folder path:", finalfolderpath);
+// });
+
+
+
+
+  
+  document.getElementById('preview').addEventListener('click', () => {
+invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/dass/src-tauri/images' })
+ 
+  });
+
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,50 @@
+use std::process::Command;
+use image_compression::dir_compression;
+use std::path::Path;
+
+// Extracts a JPEG preview image from a raw image file using exiftool.
+
+// exiftool -b -JpgFromRaw -w jpg -ext dng DIR
+// exiftool -previewimage -w _preview.jpg -ext cr2 -b DIR
+// exiftool -a -b -W %d%f_%t%-c.%s -preview:all dir
+
+#[tauri::command]
+fn extract_jpg_preview(raw_dir_file_path: String) {
+    // Run the exiftool command to extract the JPEG preview.
+    println!("extract_jpg_preview called with path: {}", raw_dir_file_path);
+    let mut dir = "./src/image2/".to_owned();
+    dir = dir + "%f_%t%-c.%s";
+
+    Command::new("exiftool")
+        .arg("-o")
+        .arg(&dir)
+        .arg("-a")
+        .arg("-b")
+        .arg("-W")
+        .arg("-previewimage")
+        .arg(&raw_dir_file_path)
+        .output()
+        .expect("Failed to execute exiftool");
+}
+
+fn main() {
+  tauri::Builder::default()
+    // This is where you pass in your commands
+    .invoke_handler(tauri::generate_handler![extract_jpg_preview])
+    .run(tauri::generate_context!())
+    .expect("failed to run app");
+     //Extract a JPEG preview image from a raw file.
+    //  let raw_dir_file_path=String::from("/Users/vanshikadhingra/dass/src-tauri/images");
+  
+    //  extract_jpg_preview(raw_dir_file_path);
+
+    // // Compress all previews extracted into image2 folder
+    // // Here goes the path where the previews are stored.
+    // let dir_string = Path::new(r"./src/image2");
+    // // Here goes the path where the compressed jpegs are stored.
+    // let dir_string2 = Path::new(r"./src/image2output");
+    // dir_compression(dir_string, dir_string2);
+
+    // println!("Preview image extracted and saved to 'preview.jpg'.");
+    
+}
