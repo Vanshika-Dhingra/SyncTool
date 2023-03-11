5c7607cccbb92e7bc90cac85d9fd39cea8dbc7a9
Vanshika Dhingra <vanshika.dhingra@students.iiit.ac.in>
2023-03-11 17:00:19 +0530

    modified index.js and index.html

--- index.js
+++ index.js
@@ -2,25 +2,59 @@
 // // Be sure to set `build.withGlobalTauri` in `tauri.conf.json` to true
 const invoke = window.__TAURI__.invoke
 const path = window.__TAURI__.path
+const dialog = window.__TAURI__.dialog
 
 // // Invoke the command
 // invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/dass/src-tauri/images' })
 
-// const finalfolderpath='';
+let inputFolderPath = '';
+let outputFolderPath = '';
 // const folderInput = document.getElementById("folder");
 // folderInput.addEventListener("change", (event) => {
 //   const folderPath = event.target.value;
-//   const folderrr=folderPath;
-//   finalfolderpath = path.dirname(folderrr);
-//   console.log("Folder path:", finalfolderpath);
-// });
+//   // const folderrr=folderPath;
+//   // inputFolderPath = path.dirname(folderrr);
+//   console.log("Folder path:",folderPath);
+//   });
 
 
 
 
-  
-  document.getElementById('preview').addEventListener('click', () => {
-invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/dass/src-tauri/images' })
- 
-  });
+
+
+document.getElementById('folder').addEventListener('click', () => {
+  //invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/OneDrive - International Institute of Information Technology/8Mar2023_dass/src-tauri/images' })
+  dialog
+    .open({
+      directory: true, // Set directory to true to allow selecting a folder instead of a file
+    })
+    .then((result) => {
+      // result contains an array of paths that the user has selected
+      inputFolderPath=result
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+document.getElementById('outFolder').addEventListener('click', () => {
+  //invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/OneDrive - International Institute of Information Technology/8Mar2023_dass/src-tauri/images' })
+  dialog
+    .open({
+      directory: true, // Set directory to true to allow selecting a folder instead of a file
+    })
+    .then((result) => {
+      // result contains an array of paths that the user has selected
+      outputFolderPath=result+'/'
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+document.getElementById('preview').addEventListener('click', () => {
+      invoke('extract_jpg_preview', { rawDirFilePath: inputFolderPath,outputFolder: outputFolderPath }, (res) => {
+        console.log(res)
+      })
+});
 
