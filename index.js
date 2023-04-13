d24d097ae6cc63c01f41cc5229622e95e6050ab1
Vanshika Dhingra <vanshika.dhingra@students.iiit.ac.in>
2023-04-13 09:44:05 +0530

    added binary

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 const invoke = window.__TAURI__.invoke
 const path = window.__TAURI__.path
 const dialog = window.__TAURI__.dialog
+const Command=window.__TAURI__.shell.Command
+// import * as walkdir from 'walkdir';
+// import * as chrono from 'chrono-node';
+// import { EventEmitter } from 'events';
+// import events from 'events';
+
+// const emitter = new EventEmitter();
 
 // // Invoke the command
 // invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/dass/src-tauri/images' })
@@ -16,11 +23,6 @@ let outputFolderPath = '';
 //   // inputFolderPath = path.dirname(folderrr);
 //   console.log("Folder path:",folderPath);
 //   });
-
-
-
-
-
 
 document.getElementById('folder').addEventListener('click', () => {
   //invoke('extract_jpg_preview', { rawDirFilePath: '/Users/vanshikadhingra/OneDrive - International Institute of Information Technology/8Mar2023_dass/src-tauri/images' })
@@ -45,7 +47,8 @@ document.getElementById('outFolder').addEventListener('click', () => {
     })
     .then((result) => {
       // result contains an array of paths that the user has selected
-      outputFolderPath=result+'/'
+      //outputFolderPath=result+'/'
+      outputFolderPath = `${result}/output%f_%t%-c.%s`;
     })
     .catch((err) => {
       console.error(err);
@@ -62,8 +65,21 @@ document.getElementById('compress').addEventListener('click', () => {
     })
 });
 
-document.getElementById('preview').addEventListener('click', () => {
-  invoke('extract_jpg_preview', { rawDirFilePath: inputFolderPath,outputFolder: outputFolderPath }, (res) => {
-    console.log(res)
-  })
+// document.getElementById('preview').addEventListener('click', async () => {
+//   console.log(outputFolderPath);
+//   const command = Command.sidecar('./exiftool_folder/blib/script/exiftool');
+//   const args = ["-a","-b","-W","/Users/vanshikadhingra/output%f","-previewimage","/Users/vanshikadhingra/images/"]
+//   const { stdout, stderr } = await command.execute(args, { stdio: ["pipe", "pipe", "pipe"] });
+//   const outputElement = document.getElementById('output');
+//   outputElement.textContent = `stdout: ${stdout}\nstderr: ${stderr}`;
+//   console.log(`stdout: ${stdout}\nstderr: ${stderr}`);
+// });
+document.getElementById('preview').addEventListener('click', async () => {  
+  console.log(outputFolderPath);
+  const command = Command.sidecar('./exiftool_folder/blib/script/exiftool',["-a","-b","-W",outputFolderPath,"-previewimage",inputFolderPath]);
+  const outputElement = command.execute()
+  console.log(outputElement)
+  // outputElement.textContent = `stdout: ${stdout}\nstderr: ${stderr}`;
+  // console.log(`stdout: ${stdout}\nstderr: ${stderr}`);
 });
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,70 +1,23 @@
-// use std::process::Command;
-// use image_compression::dir_compression;
-// use std::path::Path;
-// #[tauri::command]
-//  fn extract_jpg_preview(raw_dir_file_path: &str,output_folder: &str) {
-//     let mut diir =output_folder.to_owned();
-//     diir = diir + "%f_%t%-c.%s";
-
-//     Command::new("exiftool")
-
-//         .arg("-a")
-//         .arg("-b")
-//         .arg("-W")
-//         .arg(diir)
-
-//         .arg("-previewimage")
-//         .arg(raw_dir_file_path)
-//         .output()
-//         .map_err(|e| format!("Failed to execute exiftool: {}", e)).unwrap();
-//  }
-
-//  #[tauri::command]
-//   fn compress_jpeg(source_dir: &str, dest_dir: &str)
-// {
-//     let origin = PathBuf::from(source_dir);   // original directory path
-//     let dest = PathBuf::from(dest_dir);       // destination directory path
-//     let thread_count = 16;                       // number of threads
-//     let (tx, tr) = mpsc::channel();             // Sender and Receiver. for more info, check mpsc and message passing.
-
-//     let mut comp = FolderCompressor::new(origin, dest);
-//     comp.set_cal_func(|_width, _height, _file_size| {return Factor::new(70.0, 1.0)});
-//     comp.set_thread_count(thread_count);
-//     comp.set_sender(tx);
-
-//     match comp.compress(){
-//         Ok(_) => {},
-//         Err(e) => println!("Cannot compress the folder!: {}", e),
-//     }
-// }
-
-// fn main() {
-//   tauri::Builder::default()
-//       .invoke_handler(tauri::generate_handler![extract_jpg_preview])
-//       .run(tauri::generate_context!())
-//       .expect("failed to run app");
-// }
-use std::process::Command;
 use std::path::PathBuf;
 use std::sync::mpsc;
 use image_compressor::{FolderCompressor, Factor};
 
-#[tauri::command]
-fn extract_jpg_preview(raw_dir_file_path: &str, output_folder: &str) {
-    let mut diir = output_folder.to_owned();
-    diir = diir + "%f_%t%-c.%s";
+// #[tauri::command]
+// fn extract_jpg_preview(raw_dir_file_path: &str, output_folder: &str) {
+//     let mut diir = output_folder.to_owned();
+//     diir = diir + "%f_%t%-c.%s";
 
-    Command::new("exiftool")
-        .arg("-a")
-        .arg("-b")
-        .arg("-W")
-        .arg(diir)
-        .arg("-previewimage")
-        .arg(raw_dir_file_path)
-        .output()
-        .map_err(|e| format!("Failed to execute exiftool: {}", e))
-        .unwrap();
-}
+//     Command::new("exiftool")
+//         .arg("-a")
+//         .arg("-b")
+//         .arg("-W")
+//         .arg(diir)
+//         .arg("-previewimage")
+//         .arg(raw_dir_file_path)
+//         .output()
+//         .map_err(|e| format!("Failed to execute exiftool: {}", e))
+//         .unwrap();
+// }
 
 #[tauri::command]
 fn compress_jpeg(raw_dir_file_path: &str, output_folder: &str) {
@@ -86,7 +39,7 @@ fn compress_jpeg(raw_dir_file_path: &str, output_folder: &str) {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![extract_jpg_preview, compress_jpeg]) // Add compress_jpeg here
+        .invoke_handler(tauri::generate_handler![compress_jpeg]) // Add compress_jpeg here
         .run(tauri::generate_context!())
         .expect("failed to run app");
 }
