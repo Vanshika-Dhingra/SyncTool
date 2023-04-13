4553f32ff283adc1392f01d1bd9102ea150a251b
Vanshika Dhingra <vanshika.dhingra@students.iiit.ac.in>
2023-04-14 01:02:49 +0530

    added timestamps

--- index.js
+++ index.js
@@ -65,6 +65,110 @@ document.getElementById('compress').addEventListener('click', () => {
     })
 });
 
+// document.getElementById('changeTimestamps').addEventListener('click', () => {
+//   const selectedDate = new Date(document.getElementById("date").value);
+//   console.log(selectedDate)
+//   invoke('change_timestamps', { rawDirFilePath: inputFolderPath })
+//     .then((res) => {
+//       console.log(res)
+//     })
+//     .catch((err) => {
+//       console.error(err)
+//     })
+// });
+
+// document.getElementById('changeTimestamps').addEventListener('click', () => {
+//   // Create the date input element
+//   const dateInput = document.createElement('input');
+//   dateInput.type = 'date';
+//   dateInput.id = 'timestamps';
+//   dateInput.name = 'timestamps';
+
+//   // Add event listener to the date input element
+//   dateInput.addEventListener('change', () => {
+//     const selectedDate = dateInput.value;
+//     const final_date=new Date(selectedDate)
+//     let givendate=final_date.getFullYear();
+//     givendate=givendate+':'
+//     console.log(final_date.getMonth())
+//     console.log(final_date.get())
+//     console.log(givendate)
+//     invoke('change_timestamps', { rawDirFilePath: inputFolderPath, givendate })
+//     .then((res) => {
+//       console.log(res)
+//     })
+//     .catch((err) => {
+//       console.error(err)
+//     })
+//   });
+
+// const dateInput = document.createElement('input');
+// dateInput.type = 'text';
+// dateInput.placeholder = 'yyyy:mm:dd:hh:mm:ss';
+// dateInput.id = 'timestamps';
+// dateInput.name = 'timestamps';
+
+// // Add event listener to the date input element
+// dateInput.addEventListener('change', () => {
+//   const selectedTimestamps = dateInput.value;
+//   const [year, month, day, hour, minute, second] = selectedTimestamps.split(':');
+//   const final_date = new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`);
+//   const givendate = `${year}:${month}:${day}:${hour}:${minute}:${second}`;
+//   console.log(givendate)
+//   invoke('change_timestamps', { rawDirFilePath: inputFolderPath, givenTime:givendate })
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+// });
+const dateInput = document.createElement('input');
+dateInput.type = 'text';
+dateInput.placeholder = 'yyyy:mm:dd hh:mm:ss';  // Use spaces instead of colons
+dateInput.id = 'timestamps';
+dateInput.name = 'timestamps';
+
+// Add event listener to the date input element
+dateInput.addEventListener('change', () => {
+  const selectedTimestamps = dateInput.value.replace(/:/g, ' ');  // Replace colons with spaces
+  const [year, month, day, hour, minute, second] = selectedTimestamps.split(' ');
+  const final_date = new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`);
+  const givendate = `${year}:${month}:${day} ${hour}:${minute}:${second}`;
+  console.log(givendate)
+  invoke('change_timestamps', { rawDirFilePath: inputFolderPath, givenTime:givendate })
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+});
+
+
+// Add event listener to the changeTimestamps button
+document.getElementById('changeTimestamps').addEventListener('click', () => {
+  // Append the date input element to the document
+  document.body.appendChild(dateInput);
+});
+//n this version of the code, dateInput is defined before the event listener for the changeTimestamps button. When the button is clicked, the dateInput element is appended to the document, and the change event listener is already in place to handle any changes to the input value.
+
+
+
+
+
+
+
+document.getElementById('preview').addEventListener('click', () => {
+  invoke('extract_jpg_preview', { rawDirFilePath: inputFolderPath,outputFolder: outputFolderPath })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+});
+
 // document.getElementById('preview').addEventListener('click', async () => {
 //   console.log(outputFolderPath);
 //   const command = Command.sidecar('./exiftool_folder/blib/script/exiftool');
@@ -74,12 +178,12 @@ document.getElementById('compress').addEventListener('click', () => {
 //   outputElement.textContent = `stdout: ${stdout}\nstderr: ${stderr}`;
 //   console.log(`stdout: ${stdout}\nstderr: ${stderr}`);
 // });
-document.getElementById('preview').addEventListener('click', async () => {  
-  console.log(outputFolderPath);
-  const command = Command.sidecar('./exiftool_folder/blib/script/exiftool',["-a","-b","-W",outputFolderPath,"-previewimage",inputFolderPath]);
-  const outputElement = command.execute()
-  console.log(outputElement)
-  // outputElement.textContent = `stdout: ${stdout}\nstderr: ${stderr}`;
-  // console.log(`stdout: ${stdout}\nstderr: ${stderr}`);
-});
+// document.getElementById('preview').addEventListener('click', async () => {  
+//   console.log(outputFolderPath);
+//   const command = Command.sidecar('./exiftool_folder/blib/script/exiftool',["-a","-b","-W",outputFolderPath,"-previewimage",inputFolderPath]);
+//   const outputElement = command.execute()
+//   console.log(outputElement)
+//   // outputElement.textContent = `stdout: ${stdout}\nstderr: ${stderr}`;
+//   // console.log(`stdout: ${stdout}\nstderr: ${stderr}`);
+// });
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,118 @@
 use tauri::{
-    api::process::{Command, CommandEvent},
-    Manager,
+    api::process::{Command, CommandEvent}
   };
-  
+  use walkdir::WalkDir;
+  use chrono::TimeZone;
 use std::path::PathBuf;
 use std::sync::mpsc;
 use image_compressor::{FolderCompressor, Factor};
+
+async fn calculate_time_difference(image_path: &str, given_time: &str)-> Result<String, String> {
+  let (mut rx, mut child) = Command::new_sidecar("exiftool")
+    .expect("failed to create `my-sidecar` binary command")
+    .args(["-T","-createdate",image_path])
+    .spawn()
+    .expect("Failed to spawn sidecar");
+
+
+  let mut output_str = String::new();
+  while let Some(event) = rx.recv().await {
+    if let CommandEvent::Stdout(line) = event {
+      output_str.push_str(&line);
+    }
+  }
+  let mut image_timestamp: i64 = 0;
+    let mut given_timestamp: i64 = 0;
+
+    for line in output_str.lines() {
+        let image_time_parts: Vec<&str> = line.split_whitespace().collect();
+        let image_date_parts: Vec<&str> = image_time_parts[0].split(':').collect();
+        let image_hour_parts: Vec<&str> = image_time_parts[1].split(':').collect();
+
+        image_timestamp = chrono::Local.ymd(
+            image_date_parts[0].parse().unwrap(),
+            image_date_parts[1].parse().unwrap(),
+            image_date_parts[2].parse().unwrap(),
+        ).and_hms(
+            image_hour_parts[0].parse().unwrap(),
+            image_hour_parts[1].parse().unwrap(),
+            image_hour_parts[0].parse().unwrap(),
+        ).timestamp();
+
+        let given_time_parts: Vec<&str> = given_time.split_whitespace().collect();
+        let given_date_parts: Vec<&str> = given_time_parts[0].split(':').collect();
+        let given_hour_parts: Vec<&str> = given_time_parts[1].split(':').collect();
+
+        given_timestamp = chrono::Local.ymd(
+            given_date_parts[0].parse().unwrap(),
+            given_date_parts[1].parse().unwrap(),
+            given_date_parts[2].parse().unwrap(),
+        ).and_hms(
+            given_hour_parts[0].parse().unwrap(),
+            given_hour_parts[1].parse().unwrap(),
+            given_hour_parts[0].parse().unwrap(),
+        ).timestamp();
+    }
+
+    let diff_timestamp = image_timestamp - given_timestamp;
+    let diff_days = diff_timestamp / 86400;
+    let diff_time = chrono::Duration::seconds(diff_timestamp);
+
+    let dt = diff_time.to_std().unwrap();
+    let seconds = dt.as_secs() % 60;
+    let minutes = (dt.as_secs() / 60) % 60;
+    let hours = ((dt.as_secs() / 60) / 60) % 24;
+
+    let mut adder = String::from("");
+    adder.push_str("0000");
+    adder.push_str(":");
+    adder.push_str("00");
+    adder.push_str(":");
+    adder.push_str(&diff_days.to_string());
+    adder.push_str(" ");
+    adder.push_str(&hours.to_string());
+    adder.push_str(":");
+    adder.push_str(&minutes.to_string());
+    adder.push_str(":");
+    adder.push_str(&seconds.to_string());
+    println!("output is:{}", adder);
+    Ok(adder)
+  //Ok(output_str)
+}
+
+// Finding a raw image in the source directory to get time_diff to be added to all the other images later.
+fn find_raw_image(path: &str) -> Option<String> {
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        if let Some(ext) = entry.path().extension() {
+            if ext == "NEF" || ext == "CR2" || ext == "ARW" || ext == "RAF" || ext == "RAW" {
+                return Some(entry.path().to_string_lossy().into_owned());
+            }
+        }
+    }
+    None
+}
+
+// Adding the time difference to metadata of the raw files.
+async fn add_time_to_createdate(dir_path: &str, time_delta: &str) {
+    println!("hi");
+    let time_delta = time_delta.to_string();
+    //let dir_path = dir_path.to_string();
+    let mut adder = String::from("-AllDates+=");
+    adder.push_str(&time_delta);
+    let (mut rx, mut child) = Command::new_sidecar("exiftool")
+    .expect("failed to create `my-sidecar` binary command")
+    .args(["-r","-overwrite_orignal",&adder,dir_path])
+    .spawn()
+    .expect("Failed to spawn sidecar");
+
+  let mut output = String::new();
+  while let Some(event) = rx.recv().await {
+    if let CommandEvent::Stdout(line) = event {
+      output.push_str(&line);
+    }
+  }
+  println!("output is:{}", output);
+}
 
 #[tauri::command]
 async fn extract_jpg_preview(raw_dir_file_path: &str, output_folder: &str) -> Result<String, String> {
@@ -24,6 +131,29 @@ async fn extract_jpg_preview(raw_dir_file_path: &str, output_folder: &str) -> Re
     }
   }
   Ok(output)
+}
+
+#[tauri::command]
+async fn change_timestamps(raw_dir_file_path: &str,given_time: &str) -> Result<String, String>
+{
+    let  diffchecker = match find_raw_image(raw_dir_file_path) {
+        Some(s) => s,
+        None => String::from(""),
+    };
+
+    println!("{}\n{}", given_time, diffchecker);
+    let adder_result = calculate_time_difference(&diffchecker, given_time).await;
+let adder: String = match adder_result {
+    Ok(val) => val,
+    Err(err) => {
+        println!("Error: {}", err);
+        String::new()
+    }
+};
+    println!("This is to be added -> {}", adder);
+    add_time_to_createdate(raw_dir_file_path, &adder).await;
+    let result: Result<String, String> = Ok("Success".to_string());
+    result
 }
 
 #[tauri::command]
@@ -46,7 +176,7 @@ fn compress_jpeg(raw_dir_file_path: &str, output_folder: &str) {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![compress_jpeg,extract_jpg_preview]) // Add compress_jpeg here
+        .invoke_handler(tauri::generate_handler![change_timestamps,compress_jpeg,extract_jpg_preview]) // Add compress_jpeg here
         .run(tauri::generate_context!())
         .expect("failed to run app");
 }
